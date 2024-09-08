222ff8c1a312ffdd8a931dfe173fd929d1674b89
Nicole Tumi <tuminicole@hotmail.com>
2024-09-08 02:03:33 -0500

    forecast api integration + upd responsive styles

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ function displayTemperature(response) {
     let windElement = document.querySelector("#wind");
     let iconElement = document.querySelector("#icon");
 
-    console.log(response.data);
+    //console.log(response.data);
 
     cityElement.innerHTML = response.data.city;
 
@@ -21,6 +21,8 @@ function displayTemperature(response) {
     temperatureElement.innerHTML = temperature;
 
     iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="current-temperature-icon" />`;
+
+    getForecast(response.data.city);
 }
 
 function search(city) {
@@ -64,25 +66,47 @@ function formatDate(date) {
     return `${formattedDay} ${hours}:${minutes}`;
 }
 
-function displayForecast() {
-    let days = ["Sun", "Mon", "Tue", "Wed", "Thu"];
+function formatDay(timestamp) {
+    let date = new Date(timestamp * 1000);
+    let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+    return days[date.getDay()];
+}
+
+function getForecast(city) {
+    let apiKey = "37f3t39eac96fab30d307f44oba2bb5e";
+    let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
+    axios(apiUrl).then(displayForecast);
+}
+
+function displayForecast(response) {
+    console.log(response.data);
+
     let forecastHtml = "";
 
-    days.forEach(function (day) {
-        forecastHtml =
-            forecastHtml +
-            `
+    response.data.daily.forEach(function (day, index) {
+        if (index < 5) {
+            forecastHtml =
+                forecastHtml +
+                `
             <div class="weather-forecast-day">
-                <div class="weather-forecast-date">${day}</div>
-                <div class="weather-forecast-icon">☀️</div>
+                <div class="weather-forecast-date">${formatDay(day.time)}</div>
+                
+                    <img src="${
+                        day.condition.icon_url
+                    }" class="weather-forecast-icon"/>
+                
                 <div class="weather-forecast-temperatures">
                     <div class="weather-forecast-temp">
-                        <strong>15°</strong>
+                        <strong>${Math.round(day.temperature.maximum)}°</strong>
                     </div>
-                    <div class="weather-forecast-temp">9°</div>
+                    <div class="weather-forecast-temp">${Math.round(
+                        day.temperature.minimum
+                    )}°</div>
                 </div>
             </div>
         `;
+        }
     });
 
     let forecast = document.querySelector("#forecast");
@@ -98,4 +122,3 @@ let currentDate = new Date();
 currentDateELement.innerHTML = formatDate(currentDate);
 
 search("Lima");
-displayForecast();
